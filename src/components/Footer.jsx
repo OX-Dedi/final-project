1b0154bad66ad0ebf7f1732075a8ade44c7e0dad
Dedi <dedir8361@gmail.com>
2023-06-22 22:41:35 +0700

    final

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box className="footer">
+      <Box className="studentName">Dedi Rosadi</Box>
+      <Box className="studentId">FE5701086</Box>
+    </Box>
+  );
+};
+
+export default Footer;
